b68e358b09f90ab373ebebe6bf3da08f800942de
rukiaal <rukia.al-ani2@metropolia.fi>
2025-09-02 21:29:20 +0300

    done ,

---
+++ task03.py
@@ -0,0 +1,4 @@
+Lenght=float(input("Please enter the length of the rectangular="))
+Width=float(input("Please enter the width of the rectangular="))
+print(f"the area of the regtangular ={Lenght * Width}")
+print(f"the perimeter of rectangular ={(Lenght+Width)*2}")
