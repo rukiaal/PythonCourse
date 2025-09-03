4ba8f3fd210475589ac383541ecb96e0c98c788d
rukiaal <rukia.al-ani2@metropolia.fi>
2025-09-03 08:28:42 +0300

    done ,

---
+++ task04.py
@@ -0,0 +1,7 @@
+number1=int(input("please enter first integer number:"))
+number2=int(input("please enter second integer number:"))
+number3=int(input("please enter third integer number:"))
+print(f"the sum of the numbers={number1+number2+number3}")
+print(f"the product of the numbers={number1*number2*number3}")
+print(f"the average of the numbers={(number1+number2+number3)/3}")
+
