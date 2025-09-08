1c8b493178b58fbcc8d92329a6e9bb5f1f10c97b
rukiaal <rukia.al-ani2@metropolia.fi>
2025-09-08 14:37:05 +0300

    done ,

--- task05.py
+++ task05.py
@@ -1,7 +1,11 @@
-number1=float(input("Enter talents:"))
-number2=float(input("Enter pounds:"))
-number3=float(input("Enter lots:"))
-total_grams = (number1 * 8512) + (number2 * 425.6) + (number3 * 13.3)
+talents = int(input("Enter number of talents (leiviskä): "))
+pounds = int(input("Enter number of pounds (naula): "))
+lots = int(input("Enter number of lots (luoti): "))
+LOTS_IN_POUND = 32
+POUNDS_IN_TALENT = 20
+GRAMS_IN_LOT = 13.3
+total_lots = (talents * POUNDS_IN_TALENT * LOTS_IN_POUND) + (pounds * LOTS_IN_POUND) + lots
+total_grams = total_lots * GRAMS_IN_LOT
 kilograms = int(total_grams // 1000)
 grams = total_grams % 1000
-print((f"The weight in modern units is: {kilograms} kilograms and {grams:.2f} grams."))
+print(f"The weight in modern units is: {kilograms} kilograms and {grams:.2f} grams.")
