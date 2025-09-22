5db5d405cebd70062598df703ea2a067819b14fd
rukiaal <rukia.al-ani2@metropolia.fi>
2025-09-22 20:21:12 +0300

    done ,

--- Assignment 3/task01.py
+++ Assignment 3/task01.py
@@ -1,9 +1,8 @@
-length=float(input("Enter the length of the zander in centimeters: "))
-legal_zander_length=42.0
-if length<legal_zander_length:
-    print("The zander is too small and must be released back into the lake.")
-    short_by=legal_zander_length -length
-    print(f"{short_by:.2f} cm under the legal length")
+correct_password = "1234"
+user_password=input("enter your password: ")
 
-else:
-    print("you can take it!! ")
+while user_password != correct_password:
+    print("wrong password ,try again")
+    user_password = input("enter your password: ")
+
+print("Access granted!")
--- assignment 4/task02.py
+++ assignment 4/task02.py
@@ -1,5 +1,5 @@
-inches = float(input("enter your iches: "))
+inches = float(input("Enter your iches: "))
 while inches >= 0:
-    print(inches * 2.54 )
+    print(inches * 2.54)
     inches = float(input("enter your iches:  "))
 print("program stopped")
