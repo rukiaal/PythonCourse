757763594711525f35b05bf10881c81d9f2262c7
rukiaal <rukia.al-ani2@metropolia.fi>
2025-09-29 14:33:19 +0300

    done ,

--- assignment05/task01.py
+++ assignment05/task01.py
@@ -1,7 +1,9 @@
 import random
 dice_num = int(input("How many dice you want to roll: "))
 total = 0
+print("Rolling the dice...")
 for i in range(dice_num):
-    roll = random.randint(1,6)
+    roll = random.randint(1, 6)
+    print(f"Die {i+1}: {roll}")
     total += roll
 print(f"Sum of {dice_num} dice: {total}")
