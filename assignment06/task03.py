2d1e0f4691a672ad92199c3e7220067c043cbe38
rukiaal <rukia.al-ani2@metropolia.fi>
2025-10-04 20:02:42 +0300

    done ,

--- assignment06/task03.py
+++ assignment06/task03.py
@@ -1,20 +1,23 @@
 airports = {}
 
 while True:
-    print("\n1. Enter a new airport")
-    print("2. Fetch airport information")
-    print("3. Quit")
-    choice = input("Your choice: ")
+    choice = input("\n1 = Add, 2 = Fetch, 3 = Quit: ")
 
     if choice == "1":
-        code = input("Enter ICAO code: ")
-        name = input("Enter airport name: ")
+        code = input("ICAO code: ")
+        name = input("Airport name: ")
         airports[code] = name
+        print("Saved!")
+
     elif choice == "2":
-        code = input("Enter ICAO code:  ")
+        code = input("Enter ICAO code: ")
         if code in airports:
-            print("Airport name:", airports[code])
+            print("Airport:", airports[code])
         else:
-            print("Not found")
+            print("Not found.")
+
     elif choice == "3":
         break
+
+    else:
+        print("Invalid choice")
